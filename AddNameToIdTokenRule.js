ba267416bbb01dc30ea40f7dbb8e01632cf78a85
Henri Benoit <henri.benoit@gmail.com>
2018-06-13 12:57:18 +0200

    Fixed rule error when user_metadata is not defined

--- AddNameToIdTokenRule.js
+++ AddNameToIdTokenRule.js
@@ -1,6 +1,7 @@
 function (user, context, callback) {
-    const namespace = 'https://teamplay.siemens.com/';
-    context.idToken.family_name = user.user_metadata.family_name;
-    context.idToken.given_name = user.user_metadata.given_name;
+    if (user.user_metadata) {
+      context.idToken.family_name = user.user_metadata.family_name;
+      context.idToken.given_name = user.user_metadata.given_name;
+    }
     callback(null, user, context);
 }
